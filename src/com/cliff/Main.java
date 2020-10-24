4176c5d52ecc6a4184ad9e8d534c4a13463bab2e
Clifford Laserre <claserre9@gmail.com>
2020-10-24 19:14:54 -0400

    Memento pattern

---
+++ src/com/cliff/Main.java
@@ -0,0 +1,27 @@
+package com.cliff;
+
+import com.cliff.memento.Editor;
+import com.cliff.memento.History;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Memento pattern
+//        var editor = new Editor();
+//        var history = new History();
+//
+//
+//        editor.setContent("a");
+//        history.states.push(editor.createState());
+//
+//        editor.setContent("b");
+//        history.states.push(editor.createState());
+//
+//        editor.setContent("c");
+//
+//        editor.restore( history.states.pop());
+//        editor.restore( history.states.pop());
+//
+//        System.out.println(editor.getContent());
+    }
+}
---
+++ src/com/cliff/memento/History.java
@@ -0,0 +1,8 @@
+package com.cliff.memento;
+
+import java.util.Stack;
+
+public class History {
+    public Stack<EditorState> states = new Stack<>();
+
+}
